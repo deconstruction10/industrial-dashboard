aab2b4d9e41070ea8f012d21becae9529c087afa
deconstruction10 <andrii.barchuk@yahoo.com>
2025-02-19 21:18:42 +0200

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { reducers, metaReducers } from './store';
+import { StoreDevtoolsModule, provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideStore(reducers, {metaReducers}),
+    provideEffects(), 
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
